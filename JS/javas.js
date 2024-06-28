e2bde364e988c2cdbec26f96bfa0dc28eaac7045
CARLOSJU76 <cajugato76@hotmail.com>
2024-06-27 19:35:31 -0500

    primer commit

---
+++ JS/javas.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }else{
+        slideIndex=index;
+    }
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === slideIndex) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+// Mostrar la primera diapositiva
+showSlide(slideIndex);
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
