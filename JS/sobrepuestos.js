5e506f0d5c4b9b639d382cc477e02149a31fd784
CAJUGATO76 <cajugato76@hotmail.com>
2024-07-17 11:20:34 -0500

    actualización 17 de julio- completando log in

--- JS/sobrepuestos.js
+++ JS/sobrepuestos.js
@@ -13,3 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
       dialogOverlay.style.display = 'none';
     });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const openDialogLink = document.getElementById('open-dialog1');
+    const closeDialogLink = document.getElementById('close-dialog1');
+    const dialogOverlay = document.getElementById('dialog-overlay1');
+  
+    openDialogLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      dialogOverlay.style.display = 'flex';
+    });
+  
+    closeDialogLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      dialogOverlay.style.display = 'none';
+    });
+  });
